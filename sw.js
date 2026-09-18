1eeabec1386a10b771c891c53184838e77928c2e
Kathryn Waters <kittykat6301@gmail.com>
2026-09-18 00:24:17 -0500

    Add offline app shell caching

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'class-unknown-app-v1';
+const APP_SHELL = [
+  './ios-app.html',
+  './IHOPETHISISTHERIGHTONE.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+    )
+  );
+});
