897de3249394603ffa02afdc8d04b3e0f736c03b
Kathryn Waters <kittykat6301@gmail.com>
2026-09-18 00:26:56 -0500

    Configure Capacitor iOS app identity

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kittykat6301.classunknown',
+  appName: 'Class Unknown',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
